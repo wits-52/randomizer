4757d76dc4b3f728a72643f927ece86af0a8c1ac
ritik goyal <ritik.goyal987@gmail.com>
2021-06-13 04:55:44 +0530

    UX UI improvements

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,23 @@ let length = 2;
 let values = ['head', 'tail'];
 let tossResults = [];
 const toss = document.getElementsByClassName('toss')[0];
-
+const resultDiv = document.getElementById('result');
 toss.addEventListener('click', () => {
     tossResults = [];
-    for(let i = 0; i < 5000; i++) {
-        const index = Math.floor(Math.random() * length);
-        tossResults.push(values[index]);
-    }
-    const index = Math.floor(Math.random() * 5000);
-    // alert(tossResults[index]); 
-    document.getElementById('result').innerText = tossResults[index];
+    resultDiv.innerText = '';
+    resultDiv.style.display = 'none';
+    toss.style.display = 'none';
+    setTimeout(() => {
+        for (let i = 0; i < 5000; i++) {
+            const index = Math.floor(Math.random() * length);
+            tossResults.push(values[index]);
+        }
+        const index = Math.floor(Math.random() * 5000);
+        // alert(tossResults[index]); 
+
+        resultDiv.style.display = 'block';
+        toss.style.display = 'block';
+        resultDiv.innerText = tossResults[index];
+    }, 150);
+
 })
